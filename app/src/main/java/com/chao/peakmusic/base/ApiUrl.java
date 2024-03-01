ec7a4d0a750d7997d15f80c5fb62784b52e1f6a2
peakchao <zhangzhichaolove@vip.qq.com>
2024-03-01 09:58:49 +0800

    update api addr.

--- app/src/main/java/com/chao/peakmusic/base/ApiUrl.java
+++ app/src/main/java/com/chao/peakmusic/base/ApiUrl.java
@@ -24,7 +24,7 @@ public interface ApiUrl {
 //    Observable<HttpResult<MusicDetailsResultModel>> getMusicDetails(@Query("id") int id);
 
 
-    String BASE_URL = "http://web.peakchao.top:250/";
+    String BASE_URL = "http://home.peakchao.com:250/";
 
 
     @GET("music/getMusicBanner")
